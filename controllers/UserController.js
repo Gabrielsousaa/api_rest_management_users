721ca3b863454025e7820fca87e040722abe00ad
Gabriel de Sousa Souto <gsousax2008@Hotmail.com>
2021-07-07 13:08:06 -0300

    requição de findbyuser e findAll implementada e testa

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -41,6 +41,25 @@ class UserController {
         res.status(200);
         res.send("Tudo ok!");
     }
+
+    async edit(req, res) {
+        var { id, name, role, email } = req.body;
+        var result = await User.update(id, email, name, role);
+        if (result != undefined) {
+            if (result.status) {
+                res.status(200);
+                res.send("Tudo ok");
+            } else {
+                res.status(406);
+                res.json(result.err);
+            }
+
+
+        } else {
+            res.status(406);
+            res.send("Ocorreu um erro no servidor");
+        }
+    }
 }
 
 
